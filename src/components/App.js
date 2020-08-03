33488c194e01b5c342d0fb595b4a6b8de261c3ff
Carlos Diestro <cardiest@gmail.com>
2020-08-03 21:04:09 +0200

    Add skeleton and form dialog to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import FormDialog from './FormDialog';
 import theme from '../theme';
 import '../globalStyles.scss';
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <Typography variant="h1">Pressa</Typography>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h2" align="center">
+          Pressa
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid container item justify="flex-end" xs={12}>
+            <Button variant="contained" color="primary" onClick={handleOpen}>
+              Add card
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+      <FormDialog open={open} handleClose={handleClose} />
     </ThemeProvider>
   );
 }
