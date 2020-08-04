8a54906616ef8ca180f7136f95b4704942b889be
Carlos Diestro <cardiest@gmail.com>
2020-08-04 14:53:45 +0200

    Refactor FormDialog to add React Hook Form

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -1,46 +1,67 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 
-function FormDialog({ open, handleClose, title, description, image }) {
+function Field({ name, label, register, errors }) {
+  return (
+    <FormControl fullWidth error={errors[name] ? true : false} className="mb-1">
+      <InputLabel htmlFor={name}>{label}</InputLabel>
+      <Input id={name} name={name} inputRef={register()} />
+      <FormHelperText>
+        {errors[name] ? errors[name].message : <span>&nbsp;</span>}
+      </FormHelperText>
+    </FormControl>
+  );
+}
+
+function FormDialog({ open, handleClose }) {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Add new card</DialogTitle>
-      <DialogContent>
-        <form autoComplete="off">
-          <TextField
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+        <DialogTitle>Add new card</DialogTitle>
+        <DialogContent>
+          <Field
             name="title"
-            label={title ? title : 'Title'}
-            className="mb-2"
-            fullWidth
+            label="Title *"
+            register={() => register({ required: 'Field is required' })}
+            errors={errors}
           />
-          <TextField
+          <Field
             name="description"
-            label={description ? description : 'Description'}
-            className="mb-2"
-            fullWidth
-            multiline
+            label="Description *"
+            register={() => register({ required: 'Field is required' })}
+            errors={errors}
           />
-          <TextField
+          <Field
             name="image"
-            label={image ? image : 'Image (url)'}
-            className="mb-2"
-            fullWidth
+            label="Image (url)"
+            register={() => register()}
+            errors={errors}
           />
-        </form>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleClose} variant="contained" color="primary">
-          Save
-        </Button>
-      </DialogActions>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button variant="contained" color="primary" type="submit">
+            Save
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
