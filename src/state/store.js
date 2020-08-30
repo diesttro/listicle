2fcd2f1cbf06aff71f82eb9013244795f559a64b
Carlos Diestro <cardiest@gmail.com>
2020-08-30 19:14:01 +0200

    Add preload state to redux store

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,22 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
+const preloadState = {
+  articles: [
+    {
+      id: 'kehc4iom',
+      date: 1598806525845,
+      title: 'Demo article',
+      description: 'Feel free to edit or delete it',
+      image:
+        'https://frontendmasters.com/static-assets/learn/og-learning-path-react.jpg',
+    },
+  ],
+};
+
 const store = createStore(
   rootReducer,
+  preloadState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
